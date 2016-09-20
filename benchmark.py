5bad3402d92c949e2c11a4a91076469c48ef2547
J. Austin Wang <wangjunzhuo200@gmail.com>
2016-09-20 16:30:44 -0700

    add 80 cases

--- benchmark.py
+++ benchmark.py
@@ -5,10 +5,10 @@ import os
 
 os.system('mkdir -p results')
 
-for i in xrange(85):
+for i in xrange(165):
     os.system('cp ./cases/input{0}.txt ./input.txt'.format(i))
-    os.system('./test_target > /dev/null')
     print("-->On test case #{0}<--".format(i))
+    os.system('./test_target > /dev/null')
     os.system('diff ./output.txt ./cases/output{0}.txt'.format(i))
     os.system('cp ./output.txt ./results/output{0}.txt'.format(i))
 
